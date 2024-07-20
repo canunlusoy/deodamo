9f7535a5c39fab07e6cc07015dce1b06fed98ae1
canunlusoy <unlusoycan@gmail.com>
2024-07-21 01:00:44 +0300

    Fixes on writeability of problem types

--- src/pman/datamodels/problems.py
+++ src/pman/datamodels/problems.py
@@ -38,20 +38,36 @@ class OptimizationProblem(Problem):
 @dataclass(kw_only=True)
 class DesignProblem(Problem):
 
-    assets_to_design: list[Type[DesignAsset]]
-    asset_parameterizations: dict[Type[DesignAsset], Type[DesignParameterization]]
+    assets_to_design: list[DesignAsset]
+    asset_parameterizations: dict[DesignAsset, Type[DesignParameterization]]
 
     _data_type_str: ClassVar[str] = 'definition:problem:design'
     _data_type_key: ClassVar[int] = 8010
 
     _save_fields: ClassVar[list[str]] = Problem._save_fields + ['assets_to_design', 'asset_parameterizations']
-    _used_classes: ClassVar[list[Type['ProgramData']]] = [DesignAsset]
+    _used_classes: ClassVar[list[Type['ProgramData']]] = [DesignAsset, DesignParameterization]
 
     def __post_init__(self):
         if not all(asset_type in self.asset_parameterizations for asset_type in self.assets_to_design):
             msg = (f'A parameterization of record must be provided for all assets to be designed '
                    f'as part of the design problem must have')
             raise KeyError(msg)
+
+    def _get_raw_data(self):
+        dct = {key: getattr(self, key) for key in self._save_fields}
+        dct['asset_parameterizations'] = {asset.name: parameterization for asset, parameterization in dct['asset_parameterizations'].items()}
+        return dct
+
+    @classmethod
+    def from_dict(cls, dct: dict) -> 'DesignProblem':
+        field_data = {}
+        for key, val in dct.items():
+            if key in cls._save_fields:
+                field_data[key] = val
+
+        assets_by_name = {asset.name: asset for asset in field_data['assets_to_design']}
+        field_data['asset_parameterizations'] = {assets_by_name[asset_name]: parameterization for asset_name, parameterization in field_data['asset_parameterizations'].items()}
+        return cls(**field_data)
 
 
 @dataclass(kw_only=True)
@@ -92,14 +108,15 @@ class ConditionalGenerativeModelingProblem(GenerativeModelingProblem):
 
         return Space(input_space_params.variables + padding_vars)
 
+
 @dataclass(kw_only=True)
-class GenerativeDesignProblem(GenerativeModelingProblem, DesignProblem):
+class GenerativeDesignProblem(DesignProblem, GenerativeModelingProblem):
 
     _data_type_str: ClassVar[str] = 'definition:problem:generativeDesign'
     _data_type_key: ClassVar[int] = 8075
 
-    _save_fields: ClassVar[list[str]] = GenerativeModelingProblem._save_fields + ['design_space']
-    _used_classes: ClassVar[list[Type['ProgramData']]] = GenerativeModelingProblem._used_classes + [DesignSpace]
+    _save_fields: ClassVar[list[str]] = GenerativeModelingProblem._save_fields + DesignProblem._save_fields
+    _used_classes: ClassVar[list[Type['ProgramData']]] = list(set(GenerativeModelingProblem._used_classes + DesignProblem._used_classes + [DesignSpace]))
 
     @property
     def vars_generated(self) -> list[Variable]:
