993080051522d147bf22acc0b2816c976d62b4c0
canunlusoy <unlusoycan@gmail.com>
2024-07-21 01:02:09 +0300

    Introduce new space-to-space mapper types

--- src/datamodels/mappers.py
+++ src/datamodels/mappers.py
@@ -1,8 +1,12 @@
 from inspect import signature, getsource
 from pathlib import Path
-from typing import Any, Callable
+from typing import Any, Callable, ClassVar, Type
+from dataclasses import dataclass
 
-from src.datamodels.points import Point
+from sympy.parsing.sympy_parser import parse_expr
+
+from src.utils.iox import ProgramData
+from src.datamodels.points import Point, AbstractSpacePoint
 from src.datamodels.variables import SamplingVariable
 
 from utils.programming import is_lambda_function
@@ -17,78 +21,130 @@ class S2SMapper:
         pass
 
 
-class Equate(S2SMapper):
+@dataclass
+class SympyExpression(ProgramData, S2SMapper):
 
-    type_string = 'Equate'
+    raw_expression: str
 
-    def __init__(self, source_var: SamplingVariable):
-        self.source_var = source_var
+    _data_type_str: ClassVar[str] = 'sympy_expression'
+    _data_type_key: ClassVar[int] = '8000'
 
-    def map(self, point: Point) -> Any:
-        return point[self.source_var]
+    _save_fields: ClassVar[list[str]] = ['raw_expression']
+    _used_classes: ClassVar[list[Type['ProgramData']]] = []
 
-    def __repr__(self):
-        return f'={self.source_var.name}'
+    def __post_init__(self):
+        self.expression = parse_expr(self.raw_expression)
 
-    def to_dict(self) -> dict:
-        return {'__type__': self.type_string,
-                'source_var': self.source_var}
+    @property
+    def variables(self) -> list[str]:
+        return list([symbol.name for symbol in self.expression.free_symbols])
 
+    def evaluate(self, variable_values: dict[str, float]):
+        if not all(variable in variable_values for variable in self.variables):
+            raise KeyError('Provide values for all variables to evaluate the expression. '
+                           f'Values for some variables are missing. Required variables: {[", ".join(self.variables)]}')
 
-class FunctionReference(S2SMapper):
-
-    def __init__(self, module_path: Path, fcn_name: str):
-        pass
-
-
-
-class CustomFunctionMapper(S2SMapper):
-
-    def __init__(self, function_str: str):
-        self.function_str = function_str
-        self.function: Callable = eval(function_str)
-        self.input_arg_names = list(signature(self.function).parameters.keys())
+        result = self.expression.subs(variable_values)
+        return result
 
     def map(self, point: Point) -> Any:
-        input_arg_values = [point.get_var_value_by_name(arg_name) for arg_name in self.input_arg_names]
-        return self.function(*input_arg_values)
+        mapped = self.evaluate(variable_values={variable: value for variable, value in point.items() if variable in self.variables})
+        return mapped
 
-    @staticmethod
-    def from_lambda(lambda_fcn: Callable, available_sampling_vars: list[SamplingVariable]):
 
-        available_svar_names = [svar.name for svar in available_sampling_vars]
+@dataclass
+class FixedRangeLinearMapper(ProgramData, S2SMapper):
 
-        # Check signature
-        sig = signature(lambda_fcn)
-        arg_names = list(sig.parameters.keys())
+    fixed_range: tuple[float, float]
 
-        if not all(arg_name in available_svar_names for arg_name in arg_names):
-            invalid_arg_names = [arg_name for arg_name in arg_names if arg_name not in available_svar_names]
-            message = f'Following arguments to the lambda function do not match any sampling variables: {", ".join(invalid_arg_names)}'
+    _data_type_str: ClassVar[str] = 'fixed_range_linear_mapper'
+    _data_type_key: ClassVar[int] = '8005'
+
+    _save_fields: ClassVar[list[str]] = ['fixed_range']
+    _used_classes: ClassVar[list[Type['ProgramData']]] = []
+
+    def __post_init__(self):
+        if len(self.fixed_range) != 2:
+            message = f'Fixed range must be a tuple of 2 elements, lower and upper limits.'
             raise ValueError(message)
 
-        # Get source
-        fcn_str = getsource(lambda_fcn).split('lambda')[1]  # split at keyword ``lambda``, take right of the keyword
-        fcn_str = fcn_str.strip().replace('\n', ' ').strip(' \n ,')  # trim
-        fcn_str = f'lambda {fcn_str}'
-        try:
-            fcn_obj = eval(fcn_str)
-        except SyntaxError as error:
-            message = f'Error while processing lambda function:\n{error}'
+    def map(self,
+            point: AbstractSpacePoint,
+            dimension_index: int = None
+            ) -> float:
+
+        if dimension_index is None:
+            message = f'Index of the dimension in the abstract space point must be provided!'
             raise ValueError(message)
 
-        if not is_lambda_function(fcn_obj):
-            message = f'Provided function definition could not be resolved into a lambda function: "{fcn_str}"'
+        raw_value = point[dimension_index]
+
+        lower, upper = self.fixed_range
+        mapped_value = lower + (upper - lower) * raw_value
+
+        return mapped_value
+
+    def from_dict(cls, dct: dict) -> 'ProgramData':
+        field_data = {}
+        for key, val in dct.items():
+            if key in cls._save_fields:
+                field_data[key] = val
+
+        # Forcibly type cast to tuple - if data was read from JSON, it would have been read as list as JSON has no tuple type
+        field_data['fixed_range'] = tuple(field_data['fixed_range'])
+
+        return cls(**field_data)
+
+
+@dataclass
+class TabulatedRangeLinearMapper(ProgramData, S2SMapper):
+
+    range_lower_table_csv_fp: str | Path
+    range_upper_table_csv_fp: str | Path
+    table_rows_var: SamplingVariable
+    table_cols_var: SamplingVariable
+
+    _data_type_str: ClassVar[str] = 'tabulated_range_linear_mapper'
+    _data_type_key: ClassVar[int] = '8007'
+
+    _save_fields: ClassVar[list[str]] = ['table_csv_fp', 'dependee_sampling_vars']
+    _used_classes: ClassVar[list[Type['ProgramData']]] = []
+
+    def __post_init__(self):
+        self.range_lower_table_csv_fp = Path(self.range_lower_table_csv_fp)
+        self.range_upper_table_csv_fp = Path(self.range_upper_table_csv_fp)
+
+    def map(self,
+            point: AbstractSpacePoint,
+            dimension_index: int = None,
+            dependee_sampling_var_values: dict[SamplingVariable, float] = None
+            ) -> Any:
+
+        if dimension_index is None:
+            message = f'Index of the dimension in the abstract space point must be provided!'
             raise ValueError(message)
 
-        return CustomFunctionMapper(function_str = fcn_str)
+        if not (self.table_rows_var in dependee_sampling_var_values and self.table_cols_var in dependee_sampling_var_values):
+            message = f'Values of sampling variables representing table row and columns must be provided! Some are missing.'
+            raise KeyError(message)
 
-    def __repr__(self):
-        return f'{self.function_str}'
+        raw_value = point[dimension_index]
 
+    def _get_raw_data(self):
+        dct = {key: getattr(self, key) for key in self._save_fields}
+        for key in dct:
+            if 'fp' in key:
+                # Cast Path types as string for raw data (cannot save Path objects to JSON)
+                dct[key] = str(dct[key])
 
-class LookupMapper(S2SMapper):
+    def from_dict(cls, dct: dict) -> 'ProgramData':
+        field_data = {}
+        for key, val in dct.items():
+            if key in cls._save_fields:
+                field_data[key] = val
 
-    def __init__(self, function: Callable, lookup_table_fps: dict[str, str | Path]):
-        self.function = function
-        self.lookup_table_fps = lookup_table_fps
+        for key in field_data:
+            if 'fp' in key:
+                field_data[key] = Path(field_data[key])
+
+        return cls(**field_data)
